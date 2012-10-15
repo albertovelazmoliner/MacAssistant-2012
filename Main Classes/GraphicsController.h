3216db168783707eb2bdb6fd1136a8cf2b492eae
Thanos Siopoudis <thanoulas@gmail.com>
2012-10-15 17:27:34 +0100

    Created a GraphicsController, to load all game and extra graphics Closes #5

---
+++ Main Classes/GraphicsController.h
@@ -0,0 +1,13 @@
+//
+//  GraphicsController.h
+//  MacAssistant
+//
+//  Created by Athanasios Siopoudis on 15/10/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GraphicsController : NSObject
+
+@end
---
+++ Main Classes/GraphicsController.m
@@ -0,0 +1,13 @@
+//
+//  GraphicsController.m
+//  MacAssistant
+//
+//  Created by Athanasios Siopoudis on 15/10/2012.
+//
+//
+
+#import "GraphicsController.h"
+
+@implementation GraphicsController
+
+@end
